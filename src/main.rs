a5ef463bfeb0bbad6770db2ef4f6d17974efd158
falentio <kfalentio@proton.me>
2025-05-18 10:53:28 +0000

    chore: add debug

--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,41 @@ use esp_idf_hal::{
     task::notification::Notification,
 };
 
-fn main() -> Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+const SSID: &str = "iQOO Z7x 5G";
+const PASSWORD: &str = "123456789";
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    log::debug!("Initializing peripherals");
     let peripherals = Peripherals::take().unwrap();
 
+    log::debug!("Initializing button");
     let mut btn = PinDriver::input(peripherals.pins.gpio15)?;
     btn.set_pull(Pull::Up)?;
     btn.set_interrupt_type(InterruptType::LowLevel)?;
 
+    log::debug!("Initializing notification for button");
     let notification = Notification::new();
     let notifier = notification.notifier();
 
+    log::debug!("Subscribing to button");
     unsafe {
         btn.subscribe(move || {
             notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
         })?;
     }
 
+    log::debug!("Initializing relay");
     let mut relay = PinDriver::output(peripherals.pins.gpio13)?;
 
-    log::info!("Starting main loop");
+    log::debug!("Starting main loop");
     let is_low = AtomicBool::new(false);
     loop {
         if btn.is_low() && !is_low.load(Ordering::Relaxed) {
             relay.toggle()?;
         }
-        log::info!("Button pressed {}", btn.is_low());
         is_low.store(btn.is_low(), Ordering::Relaxed);
         FreeRtos::delay_ms(10);
     }
