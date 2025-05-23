e80a65ef851b98f071de9d88fb007d3a40119e65
falentio <kfalentio@proton.me>
2025-05-23 13:00:44 +0000

    feat: add homepage for managing relays

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::{Error, Result};
 use esp_idf_hal::{
     delay::FreeRtos,
-    gpio::{InterruptType, PinDriver, Pull},
+    gpio::{AnyIOPin, InterruptType, PinDriver, Pull},
     io::Write,
     peripherals::Peripherals,
-    task::notification::Notification,
 };
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
@@ -12,16 +11,24 @@ use esp_idf_svc::{
     nvs::EspDefaultNvsPartition,
     wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
-use std::{
-    num::NonZeroU32,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc, Mutex,
-    },
+use serde_json::json;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, Mutex,
 };
+use url::Url;
+
+use serde::Serialize;
 
 const SSID: &str = "iQOO Z7x 5G";
 const PASSWORD: &str = "123456789";
+
+#[derive(Serialize)]
+struct RelayData {
+    name: String,
+    is_active: bool,
+    id: u8,
+}
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -49,39 +56,114 @@ fn main() -> Result<()> {
     btn.set_pull(Pull::Up)?;
     btn.set_interrupt_type(InterruptType::LowLevel)?;
 
-    log::debug!("Initializing relay");
-    let relay = Arc::new(Mutex::new(PinDriver::input_output(
-        peripherals.pins.gpio13,
-    )?));
+    log::debug!("Initializing relays");
+    let relays = {
+        let mut relays = Vec::new();
+        let pin: AnyIOPin = peripherals.pins.gpio13.into();
+        relays.push(("lampu kamar", PinDriver::input_output(pin)?));
+        let pin: AnyIOPin = peripherals.pins.gpio12.into();
+        relays.push(("lampu keluarga", PinDriver::input_output(pin)?));
+        let pin: AnyIOPin = peripherals.pins.gpio14.into();
+        relays.push(("lampu ruang tamu", PinDriver::input_output(pin)?));
+        let pin: AnyIOPin = peripherals.pins.gpio27.into();
+        relays.push(("lampu dapur", PinDriver::input_output(pin)?));
+        Arc::new(Mutex::new(relays))
+    };
 
     log::debug!("Initializing server");
     let mut server = create_server()?;
-    server.fn_handler::<Error, _>("/relay/toggle", esp_idf_svc::http::Method::Post, {
-        let relay = relay.clone();
-        move |req| {
-            if let Ok(mut relay_guard) = relay.lock() {
-                relay_guard.toggle()?;
-                let is_high = relay_guard.is_high();
-                if is_high {
-                    req.into_ok_response()?.write_all("ON".as_bytes())?;
-                } else {
-                    req.into_ok_response()?.write_all("OFF".as_bytes())?;
+    server.fn_handler::<Error, _>("/", esp_idf_svc::http::Method::Get, {
+        let relays = relays.clone();
+        let relay_data = {
+            let relay_guard = relays.lock().unwrap();
+            let j = json!([
+                {
+                    "name": "lampu kamar",
+                    "isActive": relay_guard.get(0).unwrap().1.is_high(),
+                    "id": 0,
+                },
+                {
+                    "name": "lampu keluarga",
+                    "isActive": relay_guard.get(1).unwrap().1.is_high(),
+                    "id": 1,
+                },
+                {
+                    "name": "lampu ruang tamu",
+                    "isActive": relay_guard.get(2).unwrap().1.is_high(),
+                    "id": 2,
+                },
+                {
+                    "name": "lampu dapur",
+                    "isActive": relay_guard.get(3).unwrap().1.is_high(),
+                    "id": 3,
                 }
-            } else {
-                log::error!("Failed to lock relay");
-                return Err(Error::msg("Failed to lock relay"));
-            }
+            ]);
+            j.to_string()
+        };
+        let html = include_str!("../static/index.html").replace("$RELAYS", &relay_data);
+
+        move |req| {
+            req.into_ok_response()?.write_all(html.as_bytes())?;
             Ok(())
         }
     })?;
+
+    server.fn_handler::<Error, _>("/relay/toggle", esp_idf_svc::http::Method::Post, {
+        let relays = relays.clone();
+        move |req| {
+            log::info!("Relay parse req uri: {}", req.uri());
+            let u = Url::parse(format!("http:///{}", req.uri()).as_str())?;
+            let relay_id = {
+                let mut r = 0_u8;
+                if let Some((_, relay_id_str)) = u.query_pairs().find(|(k, v)| k == "relayId") {
+                    r = relay_id_str.parse::<u8>()?;
+                }
+                r
+            };
+            if relay_id > 3 {
+                req.into_response(400, Some("Relay ID is out of range"), &[])?;
+                return Ok(());
+            }
+            log::info!("Relay parse req uri: {}", relay_id);
+            let is_active = {
+                let mut r = false;
+                if let Some((_, is_active_str)) = u.query_pairs().find(|(k, v)| k == "isActive") {
+                    r = is_active_str.parse::<bool>()?;
+                }
+                r
+            };
+            log::info!("Relay parse req uri: {}", is_active);
+
+            if let Ok(mut relay_guard) = relays.lock() {
+                log::info!("Relay toggled via web");
+
+                if is_active {
+                    relay_guard
+                        .get_mut(relay_id as usize)
+                        .unwrap()
+                        .1
+                        .set_high()?;
+                } else {
+                    relay_guard
+                        .get_mut(relay_id as usize)
+                        .unwrap()
+                        .1
+                        .set_low()?;
+                }
+            }
+            req.into_ok_response()?;
+            Ok(())
+        }
+    })?;
+
     log::info!("Starting main loop");
     let is_low = AtomicBool::new(false);
     loop {
         if btn.is_low() && !is_low.load(Ordering::Relaxed) {
-            if let Ok(mut relay_guard) = relay.lock() {
-                relay_guard.toggle()?;
-                log::info!("Relay toggled via button");
-            }
+            // if let Ok(mut relay_guard) = relays.lock() {
+            //     relay_guard.toggle()?;
+            //     log::info!("Relay toggled via button");
+            // }
         }
         is_low.store(btn.is_low(), Ordering::Relaxed);
         // TODO: auto reconnect to wifi
